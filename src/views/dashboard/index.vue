45a34316a4ab1f0fe6b92e78b135e93a9ae48070
布丁的老父亲 <ruiwei88888@163.com>
2020-04-26 11:51:42 +0800

    update

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">Hello world</div>
   </div>
 </template>
 
@@ -10,21 +10,42 @@ import { mapGetters } from 'vuex'
 export default {
   name: 'Dashboard',
   computed: {
-    ...mapGetters([
-      'name'
-    ])
+    ...mapGetters(['name'])
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@font-face {
+  font-family: hello;
+  src: url(https://m0-file2.bybutter.com/uploaded/rms/0FCC0AC2C38D38A6A411587632739091.otf);
+}
 .dashboard {
   &-container {
-    margin: 30px;
+    width: 100%;
+    height: calc(100vh - 50px);
+    background: #0f2027; /* fallback for old browsers */
+    background: -webkit-linear-gradient(
+      to top,
+      #2c5364,
+      #203a43,
+      #0f2027
+    ); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(
+      to top,
+      #2c5364,
+      #203a43,
+      #0f2027
+    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
   &-text {
-    font-size: 30px;
-    line-height: 46px;
+    color: #fff;
+    font-family: hello;
+    font-size: 3rem;
   }
 }
 </style>
